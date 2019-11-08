24461a1846121f937c943a2ce013c03ee17c1bc8
PlebeyRisk <tuff546@gmail.com>
2019-11-08 19:36:50 +0300

    Updates

---
+++ precache-manifest.ee0fe5eb93dae32e6b9c98b59a6fb2c8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2355577811e04cd2aed5735232ed7a29",
+    "url": "/react-organization-test/index.html"
+  },
+  {
+    "revision": "b5c247e3d37cb8538b04",
+    "url": "/react-organization-test/static/js/2.7cfab4fe.chunk.js"
+  },
+  {
+    "revision": "5295ab3a9092ccb8fd6d",
+    "url": "/react-organization-test/static/js/main.7d3276f4.chunk.js"
+  },
+  {
+    "revision": "360befc017656b74f55f",
+    "url": "/react-organization-test/static/js/runtime-main.01cf16ed.js"
+  },
+  {
+    "revision": "260d1a86f3ec3401ce63c16a987bc48c",
+    "url": "/react-organization-test/static/media/add-new.260d1a86.svg"
+  },
+  {
+    "revision": "2d39936d92d910fbc38ff6573d91f889",
+    "url": "/react-organization-test/static/media/delete.2d39936d.svg"
+  },
+  {
+    "revision": "7731927ed0df6c231e76b1c2c101ee4f",
+    "url": "/react-organization-test/static/media/logo.7731927e.svg"
+  },
+  {
+    "revision": "1debe510eaec250090d34b371d6c7295",
+    "url": "/react-organization-test/static/media/more-mark.1debe510.svg"
+  },
+  {
+    "revision": "a45049f83bb4022a979c1a36277cd0f0",
+    "url": "/react-organization-test/static/media/success-mark.a45049f8.svg"
+  }
+]);
